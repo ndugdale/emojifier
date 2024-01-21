d638a2f7a8b9fe78df54979a494329dc8f516602
ndugdale <67626400+ndugdale@users.noreply.github.com>
2024-01-21 16:59:00 +1100

    feat: init react and tailwindcss

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="w-screen h-screen flex flex-col justify-center items-center" >
+      <div className="font-bold">Hello World</div>
+    </div>
+  );
+}
+
+export default App;
